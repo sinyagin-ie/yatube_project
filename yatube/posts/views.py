cbb8d3c88ba2ee2e3ae6dea14758487435e2bf94
Иван Синягин~ <sinyagine@yandex.ru>
2023-02-23 12:51:13 +0300

    Works, without PEP8.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,8 +4,10 @@ from .models import Post, Group
 
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
     context = {
         'posts': posts,
+        'title': title,
     }
     return render(request, 'posts/index.html', context)
 
@@ -13,8 +15,10 @@ def index(request):
 def group_post(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = f'Записи сообщества {group}'
     context = {
         'group': group,
         'posts': posts,
+        'title': title,
     }
     return render(request, 'posts/group_list.html', context)
